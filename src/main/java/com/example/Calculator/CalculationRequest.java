d5ecd01f1b7373b8f77a406eafd48d95d81d8918
Aditya-divyam <adi.divyam@gmail.com>
2025-06-08 15:16:35 +0530

    Initial commit

---
+++ src/main/java/com/example/Calculator/CalculationRequest.java
@@ -0,0 +1,32 @@
+package com.example.Calculator;
+
+public class CalculationRequest {
+    private double a;
+    private double b;
+    private String operator;
+
+    // Getters and setters
+    public double getA() {
+        return a;
+    }
+
+    public void setA(double a) {
+        this.a = a;
+    }
+
+    public double getB() {
+        return b;
+    }
+
+    public void setB(double b) {
+        this.b = b;
+    }
+
+    public String getOperator() {
+        return operator;
+    }
+
+    public void setOperator(String operator) {
+        this.operator = operator;
+    }
+}
